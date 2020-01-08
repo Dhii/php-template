206e790137842abbeb8b7d468e28fa326d251f04
Anton Ukhanev <xedin.unknown@gmail.com>
2020-01-08 11:14:11 +0100

    Now testing rendering with all possible types of context

--- test/functional/Template/PhpTemplateTest.php
+++ test/functional/Template/PhpTemplateTest.php
@@ -2,12 +2,18 @@
 
 namespace Dhii\Output\FuncTest\Template;
 
+use ArrayAccess;
+use ArrayObject;
 use Dhii\Output\PhpEvaluator\PhpEvaluatorInterface;
 use Exception;
 use PHPUnit\Framework\TestCase;
 use Dhii\Output\Template\PhpTemplate as TestSubject;
 use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use Psr\Container\ContainerInterface;
 
+/**
+ * Tests `PhpTemplate`.
+ */
 class PhpTemplateTest extends TestCase
 {
     /**
@@ -43,11 +49,88 @@ class PhpTemplateTest extends TestCase
         return $mock;
     }
 
-    public function testRender()
+    /**
+     * Creates a new array access object.
+     *
+     * @param array $data The data for the array access object.
+     *
+     * @return ArrayAccess|MockObject The new instance of an array access object.
+     */
+    public function createArrayAccess(array $data): ArrayAccess
+    {
+        $mock = $this->getMockBuilder(ArrayObject::class)
+            ->setConstructorArgs([$data])
+            ->setMethods(null)
+            ->getMock();
+
+        return $mock;
+    }
+
+    /**
+     * Creates a new container.
+     *
+     * @param array $data The data for the container.
+     *
+     * @return ContainerInterface|MockObject
+     */
+    public function createContainer(array $data): ContainerInterface
+    {
+        $mock = $this->getMockBuilder(ContainerInterface::class)
+            ->setMethods(['get', 'has'])
+            ->getMock();
+
+        $mock->method('get')
+            ->will($this->returnCallback(function(string $key) use ($data) {
+                if (!isset($data[$key])) {
+                    return null;
+                }
+
+                return $data[$key];
+            }));
+
+        $mock->method('has')
+            ->will($this->returnCallback(function (string $key) use ($data) {
+                return array_key_exists($key, $data);
+            }));
+
+        return $mock;
+    }
+
+    /**
+     * Provides data useful for testing rendering with different contexts.
+     *
+     * @return array<int, array> The data, with following members.
+     *                           0. A key.
+     *                           1. A value.
+     *                           2. A context that contains the key which corresponds to the value.
+     */
+    public function renderDataProvider(): array
+    {
+        $key = uniqid('context-key');
+        $value = uniqid('context-value');
+        $data = [$key => $value];
+        return [
+            [$key, $value, $data],
+            [$key, $value, $this->createArrayAccess($data)],
+            [$key, $value, $this->createContainer($data)],
+        ];
+    }
+
+    /**
+     * Tests that rendering works correctly.
+     *
+     * @dataProvider renderDataProvider
+     *
+     * @param $keyExisting string A name of a key that exists in the context.
+     * @param $valueExisting string A value that corresponds to the existing key in the context.
+     * @param $context array|ArrayAccess|ContainerInterface The context.
+     *
+     * @throws \Dhii\Output\Exception\RendererExceptionInterface
+     * @throws \Dhii\Output\Exception\TemplateRenderExceptionInterface
+     */
+    public function testRender(string $keyExisting, string $valueExisting, $context)
     {
         {
-            $keyExisting = uniqid('key-existing');
-            $valueExisting = uniqid('val-existing');
             $keyDefault = uniqid('key-default');
             $valueDefault = uniqid('val-default');
             $defaultContext = [
@@ -68,9 +151,7 @@ class PhpTemplateTest extends TestCase
         }
 
         {
-            $result = $subject->render([
-                $keyExisting => $valueExisting,
-            ]);
+            $result = $subject->render($context);
 
             $this->assertEquals($valueExisting . $valueDefault . $funcResultSuffix, $result, 'Rendering subject produced unexpected result');
         }
